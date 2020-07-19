740d20a6f55970be26b07dc9725efa626239e50c
Alex Shyshkov <shyshkov.alex@gmail.com>
2020-07-19 23:07:25 +0300

    Added calculation

--- 02-homework/script.js
+++ 02-homework/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+let firstNumber = +prompt('Enter first numder', ''),
+    secondNumber = +prompt('Enter second number', ''),
+    result = 0;
+
+if(!isNaN(firstNumber) && !isNaN(secondNumber)) {
+    if(confirm('Do you want to skip the pair numbers?')){
+        for(let i = firstNumber; i <= secondNumber; i++){
+            if(i % 2 == 0) {
+                continue;
+            }
+            result += i;
+        }        
+    } else {
+        for(let i = firstNumber; i <= secondNumber; i++){
+            result += i;
+        }
+    }
+    console.log(`Result is ${result}`);
+} else {
+    alert('Incorrect data type. Try again');
+}
