619aa383d53a8afd971f129674abbbaae20951f4
Alex Shyshkov <shyshkov.alex@gmail.com>
2020-07-22 21:47:03 +0300

    Changed user confirmation and check for the pair numbers

--- 02-homework/script.js
+++ 02-homework/script.js
@@ -2,21 +2,17 @@
 
 let firstNumber = +prompt('Enter first numder', ''),
     secondNumber = +prompt('Enter second number', ''),
+    isSkipPairNumbers = confirm('Do you want to skip the pair numbers?'),
     result = 0;
 
 if(!isNaN(firstNumber) && !isNaN(secondNumber)) {
-    if(confirm('Do you want to skip the pair numbers?')){
-        for(let i = firstNumber; i <= secondNumber; i++){
-            if(i % 2 == 0) {
-                continue;
-            }
-            result += i;
-        }        
-    } else {
-        for(let i = firstNumber; i <= secondNumber; i++){
-            result += i;
+    for(let i = firstNumber; i <= secondNumber; i++){
+        if(i % 2 == 0 && isSkipPairNumbers === true) {
+            continue;
         }
-    }
+        result += i;
+    }        
+
     console.log(`Result is ${result}`);
 } else {
     alert('Incorrect data type. Try again');
