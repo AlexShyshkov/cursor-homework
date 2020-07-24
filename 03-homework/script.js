bcbca1496ee459d9d2f8543547e9a0e6b45b65b1
Alex Shyshkov <shyshkov.alex@gmail.com>
2020-07-24 22:46:41 +0300

    Added getPow function

--- 03-homework/script.js
+++ 03-homework/script.js
@@ -14,4 +14,14 @@ function getMaxDigit(number) {
     return maxDigit;
 }
 
+function getPow(value, power) {
+    let result = value;
+    for (let i = 1; i < power; i++) {
+        result *= value;        
+    }
+
+    return result;
+}
+
 document.writeln(`Функція №1: ${getMaxDigit(123845)}`);
+document.writeln(`Функція №2: ${getPow(3, 3)}`);
