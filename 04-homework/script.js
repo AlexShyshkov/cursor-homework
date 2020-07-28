a954682a57d4204c930ceda38185224ecdd738aa
Alex Shyshkov <shyshkov.alex@gmail.com>
2020-07-28 22:29:57 +0300

    Added swapStudentsOrder function and getPairs function

--- 04-homework/script.js
+++ 04-homework/script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
+const themes = ["Диференційне рівняння", "Теорія автоматів", "Алгоритми і структури даних"];
+const marks = [4, 5, 5, 3, 4, 5];
+
+function swapStudentsOrder(arr, from, to) {
+    arr[from] = arr.splice(to, 1, arr[from])[0];
+}
+
+swapStudentsOrder(students, 1, 2);
+//console.log(students);
+
+function getPairs(arr) {
+    const studentPairs = [];
+    for (let i = 0; i < arr.length; i+=2) {
+        studentPairs.push(new Array(arr[i], arr[i+1]));
+    }
+    return studentPairs;
+  }
+
+const pairs = getPairs(students);
+
+console.log(pairs);
