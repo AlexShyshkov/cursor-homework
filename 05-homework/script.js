7e9990a87b0ff29aa17de2ec0ee99a5d5b3f22d0
Alex Shyshkov <shyshkov.alex@gmail.com>
2020-08-01 16:04:23 +0300

    Added getRandomArray function

--- 05-homework/script.js
+++ 05-homework/script.js
@@ -1,2 +1,11 @@
 "use strict";
 
+function getRandomArray(length, min, max) {
+    let newArr = [];
+    for (let i = 0; i < length; i++) {
+        newArr.push(Math.floor(Math.random() * (max - min) + min));
+    }
+    return newArr;
+}
+
+console.log(getRandomArray(10, 2, 55));
