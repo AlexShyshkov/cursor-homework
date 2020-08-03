61dc3185538e18b16d45f1ad2a0951e00e092a3f
Alex Shyshkov <shyshkov.alex@gmail.com>
2020-08-03 20:47:39 +0300

    Added getMedian function

--- 05-homework/script.js
+++ 05-homework/script.js
@@ -19,6 +19,15 @@ function getAverage(...numbers) {
                     }, 0) / numbers.length;
 }
 
+function getMedian(...numbers) {
+    let arr = numbers.sort((a, b) => a - b);
+    if(arr.length % 2) {
+        return arr[Math.floor(arr.length / 2)];
+    } else {
+        return (arr[arr.length /2] + arr[arr.length / 2 - 1]) / 2;
+    }
+}
+
 function filterEvenNumbers(...numbers) {
     return numbers.filter(numbers => numbers % 2 !==0);
 }
@@ -33,6 +42,7 @@ function getDivideByFive(...numbers) {
 
 console.log(getRandomArray(10, 2, 55));
 console.log(getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
+console.log(getMedian(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
 console.log(filterEvenNumbers(1, 2, 3, 4, 5));
 console.log(countPositiveNumbers(1, -3, 5, 6, -36));
 console.log(getDivideByFive(25, 5, 2, 9));
