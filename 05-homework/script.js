23d0cb82c03339c5627066d32510bc94491d013e
Alex Shyshkov <shyshkov.alex@gmail.com>
2020-08-03 19:05:24 +0300

    Added filterEvenNumbers function

--- 05-homework/script.js
+++ 05-homework/script.js
@@ -19,5 +19,10 @@ function getAverage(...numbers) {
                     }, 0) / numbers.length;
 }
 
+function filterEvenNumbers(...numbers) {
+    return numbers.filter(numbers => numbers % 2 !==0);
+}
+
 console.log(getRandomArray(10, 2, 55));
 console.log(getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
+console.log(filterEvenNumbers(1, 2, 3, 4, 5));
