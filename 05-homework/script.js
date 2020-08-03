a17293cba069ca1f426d468370f93415673bb086
Alex Shyshkov <shyshkov.alex@gmail.com>
2020-08-03 21:51:17 +0300

    Added replaceBadWords function

--- 05-homework/script.js
+++ 05-homework/script.js
@@ -9,7 +9,20 @@ function getRandomArray(length, min, max) {
 }
 
 /*function getModa(...numbers) {
-
+    let num = numbers.filter(number => Number.isInteger(number));
+    let count = 0;
+    let max = num[0];
+    for (let i = 0; i < num.length; i++) {
+        if(max < num[i]) {
+            max = num[i];
+        }        
+    }
+    for (let i = 0; i < num.length; i++){
+        if(num[i] == max) {
+            count++;
+        }
+    }
+    return count;
 }*/
 
 function getAverage(...numbers) {
@@ -40,9 +53,26 @@ function getDivideByFive(...numbers) {
     return numbers.filter(numbers => numbers %5 == 0);
 }
 
+let censoredWords = ["shit", "fuck"];
+
+function replaceBadWords(text, censored) {
+    return text.split(" ")
+                .map(modifiedWord => {
+                    for(let i = 0; i < censored.length; i++) {
+                        if(modifiedWord.toLowerCase().includes(censored[i])) {
+                            return modifiedWord.replace(censored[i], "*".repeat(censored[i].length));
+                        }
+                    }
+                    return modifiedWord;
+                })
+                .join(" ");
+}
+
 console.log(getRandomArray(10, 2, 55));
+//console.log(getModa(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
 console.log(getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
 console.log(getMedian(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
 console.log(filterEvenNumbers(1, 2, 3, 4, 5));
 console.log(countPositiveNumbers(1, -3, 5, 6, -36));
 console.log(getDivideByFive(25, 5, 2, 9));
+console.log(replaceBadWords("Are you fucking kidding?", censoredWords));
