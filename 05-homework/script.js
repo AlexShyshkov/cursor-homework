3de6761b8edd887c6543bea729a008fe282a5ec7
Alex Shyshkov <shyshkov.alex@gmail.com>
2020-08-03 19:44:27 +0300

    Added getDivideByFive function

--- 05-homework/script.js
+++ 05-homework/script.js
@@ -27,7 +27,12 @@ function countPositiveNumbers(...numbers) {
     return numbers.filter(numbers => numbers > 0).length;
 }
 
+function getDivideByFive(...numbers) {
+    return numbers.filter(numbers => numbers %5 == 0);
+}
+
 console.log(getRandomArray(10, 2, 55));
 console.log(getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
 console.log(filterEvenNumbers(1, 2, 3, 4, 5));
 console.log(countPositiveNumbers(1, -3, 5, 6, -36));
+console.log(getDivideByFive(25, 5, 2, 9));
