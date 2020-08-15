c74337e19cbddda3b746e3d6a921eae779acd4e0
Alex Shyshkov <shyshkov.alex@gmail.com>
2020-08-15 21:24:53 +0300

    Added dismissStudent method in Student object

--- 08-homework/script.js
+++ 08-homework/script.js
@@ -5,7 +5,9 @@ class Student {
         this.university = university;
         this.course = course;
         this.fullName = fullName;
+
         this._marks = [5, 4, 4, 5];
+        this._isDismissed = false;
     }
 
     getInfo() {
@@ -24,6 +26,15 @@ class Student {
         return this.marks.reduce((accumulator, value) => accumulator + value) / this._marks.length
                          .toFixed(2);
     }
+
+    dismissStudent() {
+        if (this._isDismissed == false) {
+            this._isDismissed = true;
+            console.log(`${this.fullName} dismissed`);
+        } else {
+            console.log(`${this.fullName} has alredy dismissed!`);
+        }
+    }
 }
 
 let vitya = new Student("Oxford", 2, " Vitya Alvarez");
@@ -33,3 +44,5 @@ console.log(vitya.marks);
 console.log(vitya.marks = 5);
 console.log(vitya.marks);
 console.log(vitya.getAverageMark());
+console.log(vitya.dismissStudent());
+console.log(vitya.dismissStudent());
