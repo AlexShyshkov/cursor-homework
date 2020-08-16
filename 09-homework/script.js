6701f366628380d54bdc6fda29511f044ec3342c
Alex Shyshkov <shyshkov.alex@gmail.com>
2020-08-16 17:15:37 +0300

    Created generateBlock function. Modified css styles of container element

--- 09-homework/script.js
+++ 09-homework/script.js
@@ -1,2 +1,21 @@
 "use strict";
 
+window.addEventListener("DOMContentLoaded", function() {
+    function getRandomColour() {
+        
+    }
+    
+    function generateBlocks() {
+        let container = document.createElement('div');
+        container.classList.add('container');
+        document.body.prepend(container);
+
+        for (let i = 0; i < 25; i++) {
+            let block = document.createElement('div');
+            block.classList.add('block');
+            container.appendChild(block);
+        }
+    }
+    
+    generateBlocks();
+});
