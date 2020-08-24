afdc4e4c641942e6b0f395155e0600851330a357
Alex Shyshkov <shyshkov.alex@gmail.com>
2020-08-24 18:29:43 +0300

    Added tooltip property in index.html. Created tooltip.css file. Added mouseover and mouseout events for button icon's tooltips

--- 10-homework/script.js
+++ 10-homework/script.js
@@ -3,9 +3,46 @@
 window.addEventListener("DOMContentLoaded", function(){
     let start = document.getElementById("start");
     let buttons = document.getElementById("buttons");
+    let tooltip;
     
     start.addEventListener("click", function(){
         start.style.display = "none";
         buttons.style.display = "block";
+    });
+
+    document.addEventListener("mouseover", function(event) {
+        let target = event.target;
+
+        let tooltipHtml = target.dataset.tooltip;
+        if (!tooltipHtml) return;
+
+        tooltip = document.createElement("div");
+        tooltip.className = "tooltip";
+        tooltip.innerHTML = tooltipHtml;
+        document.body.append(tooltip);
+
+        let position = target.getBoundingClientRect();
+
+        let left = position.left + (target.offsetWidth - tooltip.offsetWidth) / 2;
+
+        if(left < 0) {
+            left = 0;
+        }
+
+        let top = position.top - tooltip.offsetHeight - 5;
+
+        if(top < 0) {
+            top = position.top + target.offsetHeight + 5;
+        }
+
+        tooltip.style.left = left + "px";
+        tooltip.style.top = top + "px";
+    });
+
+    document.addEventListener("mouseout", function(e) {
+        if(tooltip) {
+            tooltip.remove();
+            tooltip = null;
+        }
     });
 });
