0ec90cb8991e29154ed7787a33aba5b19a2fd1c2
Alex Shyshkov <shyshkov.alex@gmail.com>
2020-08-24 17:47:12 +0300

    Added styles for buttons panel

--- 10-homework/script.js
+++ 10-homework/script.js
@@ -1,10 +1,11 @@
 "use strict";
 
 window.addEventListener("DOMContentLoaded", function(){
-
     let start = document.getElementById("start");
+    let buttons = document.getElementById("buttons");
     
     start.addEventListener("click", function(){
         start.style.display = "none";
+        buttons.style.display = "block";
     });
 });
