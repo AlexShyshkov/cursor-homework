ff105ac8a16180880d839f568212097465cbdb3d
Alex Shyshkov <shyshkov.alex@gmail.com>
2020-08-29 21:40:49 +0300

    Added check fo length value for promise

--- 11-homework/script.js
+++ 11-homework/script.js
@@ -2,11 +2,16 @@
 
 function getRandomChinese(length) {
     return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            const sign = Date.now().toLocaleString("zh-Hans-CN-u-nu-hanidec").slice(-5);
-            resolve(console.log(sign));
-        }, length * 50);
+        if(length <= 0) {
+            reject("Length less then 0");
+        } else {
+            setTimeout(() => {
+                const sign = Date.now().toLocaleString("zh-Hans-CN-u-nu-hanidec").slice(-5);
+                resolve(console.log(sign));
+            }, length * 50);
+        }
     });    
 }
 
+getRandomChinese(0);
 getRandomChinese(4);
