8f8b0675c560b0f1275ef8c422d0436043fa48b5
Alex Shyshkov <shyshkov.alex@gmail.com>
2020-08-29 21:11:18 +0300

    Function returnedr 5 last signs of Date as chinese glyphs

--- 11-homework/script.js
+++ 11-homework/script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+function getRandomChinese() {
+    const sign = Date.now().toLocaleString("zh-Hans-CN-u-nu-hanidec").slice(-5);
+    //const text = String.fromCharCode(sign);
+    return sign;
+}
+
+//getRandomChinese(4);
+console.log(getRandomChinese());
