3301ab4af144c29ff22cbbf858a1e42128fe8073
Alex Shyshkov <shyshkov.alex@gmail.com>
2020-08-29 21:57:58 +0300

    Added timer for promise executing time

--- 11-homework/script.js
+++ 11-homework/script.js
@@ -1,16 +1,20 @@
 "use strict";
 
 function getRandomChinese(length) {
+    let promiseStartTime = Date.now();
     return new Promise((resolve, reject) => {
         if(length <= 0) {
             reject("Length less then 0");
         } else {
             setTimeout(() => {
                 const sign = Date.now().toLocaleString("zh-Hans-CN-u-nu-hanidec").slice(-5);
-                resolve(console.log(sign));
+                resolve(sign);
             }, length * 50);
         }
-    });    
+    }).then(sign => {
+            console.log(`Result ${sign} for ${Date.now() - promiseStartTime} ms`);
+        }        
+    );
 }
 
 getRandomChinese(0);
