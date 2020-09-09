55e38de89a5ca2011c5c861155eedfd46ff2e2a5
Alex Shyshkov <shyshkov.alex@gmail.com>
2020-09-09 21:31:21 +0300

    Created panel with info buttons

--- 12-homework/script.js
+++ 12-homework/script.js
@@ -3,6 +3,9 @@
 window.addEventListener("DOMContentLoaded", function(){
     let start = document.getElementById("start");
     let crawl = document.getElementById("sw");
+    let infoPanel = document.getElementById("info");
+    let hero = document.getElementById("hero");
+    let planet = document.getElementById("planet");
 
     function playSound(path){
         let sound = document.createElement("audio");
@@ -10,16 +13,26 @@ window.addEventListener("DOMContentLoaded", function(){
         sound.volume = 0.3;
         sound.src = `src/sound/${path}`;
         sound.play();
+        setTimeout(() => {
+            sound.pause();
+        }, 60000);
     }
 
     function initApp(){
         start.style.display = "none";
         crawl.style.display = "flex";
         playSound("main.mp3");
+
+        setTimeout(() => {
+            infoPanel.style.display = "flex";
+        }, 40000);
     }
 
-    start.addEventListener("mouseover", () => {
+    start.addEventListener("click", initApp);
+    hero.addEventListener("mouseover", () => {
         playSound("sword.mp3");
     });
-    start.addEventListener("click", initApp);
+    planet.addEventListener("mouseover", () => {
+        playSound("sword.mp3");
+    });
 });
